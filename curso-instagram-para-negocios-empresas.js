9a5187649f979dec70d3e8ebc9b57f65385f0bb7
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-04-10 12:16:45 -0600

    1
    1

---
+++ curso-instagram-para-negocios-empresas.js
@@ -0,0 +1,42 @@
+function videos(e){
+var video = ["/",
+"https://www.youtube.com/embed/aLyWMVwnSJc?controls=0"];
+
+function ads(){
+
+var externalScript   = document.createElement("script");
+externalScript.type  = "text/javascript";
+externalScript.setAttribute('async',"");
+externalScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+document.getElementById('adsContenedor').appendChild(externalScript);
+
+var comment = document.createComment(" info-cursos 300x250 ");
+document.getElementById('adsContenedor').appendChild(comment);
+
+var ins   = document.createElement("ins");
+ins.setAttribute('class','adsbygoogle');
+ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
+ins.setAttribute('data-ad-slot','4632773522');
+ins.setAttribute('style','display:inline-block;width:300px;height:250px');
+document.getElementById('adsContenedor').appendChild(ins);
+
+var inlineScript   = document.createElement("script");
+inlineScript.type  = "text/javascript";
+inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
+document.getElementById('adsContenedor').appendChild(inlineScript); 
+
+}
+
+Swal.fire({
+title: '<strong>Curso Instagram para negocios empresas gratis -
+herramientas, campañas, recomendaciones</strong>', 
+html:  '¿Te gustó?</br>¡compártelo con tus amigos y familiares!' +
+    '</br></br><div class="iframe-container"><iframe title="vimeo-player" src=' + video[e] + ' width="100%" height="480" frameborder="0" allowfullscreen></iframe></div></br><div id="adsContenedor"></div></br>',
+showCloseButton: true,
+showConfirmButton: false,
+focusCancel: false,
+focusConfirm: false,
+allowOutsideClick: false
+})
+ads();
+}
