d0060f6b07bd53b0bb612ff1452f523f1aa9d7bf
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-15 15:46:22 -0600

    1
    1

--- curso-carpinteria.js
+++ curso-carpinteria.js
@@ -42,14 +42,16 @@ externalScript.setAttribute('async',"");
 externalScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
 document.getElementById('adsContenedor').appendChild(externalScript);
 
-var comment = document.createComment(" info-cursos 300x250 ");
+var comment = document.createComment(" infocursos_adap_jul ");
 document.getElementById('adsContenedor').appendChild(comment);
 
 var ins   = document.createElement("ins");
 ins.setAttribute('class','adsbygoogle');
+ins.setAttribute('style','display:block');
 ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
-ins.setAttribute('data-ad-slot','4632773522');
-ins.setAttribute('style','display:inline-block;width:300px;height:250px');
+ins.setAttribute('data-ad-slot','9303445348');
+ins.setAttribute('data-ad-format','auto');
+ins.setAttribute('data-full-width-responsive','true');
 document.getElementById('adsContenedor').appendChild(ins);
 
 var inlineScript   = document.createElement("script");
