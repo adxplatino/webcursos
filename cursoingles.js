380542a5cf2ba5d5b6b1369a025439d25af9263f
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-18 21:39:33 -0600

    22
    22

--- cursoingles.js
+++ cursoingles.js
@@ -11,33 +11,6 @@ var leccion = ["/",
 "https://player.vimeo.com/video/392907832?title=0&byline=0&portrait=0&sidedock=0",
 "https://player.vimeo.com/video/392909060?title=0&byline=0&portrait=0&sidedock=0"];
 
-function ads(){
-
-var externalScript   = document.createElement("script");
-externalScript.type  = "text/javascript";
-externalScript.setAttribute('async',"");
-externalScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-document.getElementById('adsContenedor').appendChild(externalScript);
-
-var comment = document.createComment(" infocursos_adap_jul ");
-document.getElementById('adsContenedor').appendChild(comment);
-
-var ins   = document.createElement("ins");
-ins.setAttribute('class','adsbygoogle');
-ins.setAttribute('style','display:block');
-ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
-ins.setAttribute('data-ad-slot','9303445348');
-ins.setAttribute('data-ad-format','auto');
-ins.setAttribute('data-full-width-responsive','true');
-document.getElementById('adsContenedor').appendChild(ins);
-
-var inlineScript   = document.createElement("script");
-inlineScript.type  = "text/javascript";
-inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
-document.getElementById('adsContenedor').appendChild(inlineScript); 
-
-}
-
 Swal.fire({
 title: '<strong>Leccion ' + e + '</strong>', 
 html:  'Disfruta nuestro fantástico curso en vídeo completamente gratis (nivel básico). Y si te gusta, ¡compártelo con tus amigos en las redes sociales!' +
@@ -48,5 +21,5 @@ focusCancel: false,
 focusConfirm: false,
 allowOutsideClick: false
 })
-ads();
+
 }
