85e30d9e4d27b4f02e906697373243d5329fb588
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-19 06:51:55 -0600

    11
    11

--- conservar_comida_video.js
+++ conservar_comida_video.js
@@ -1,16 +1,10 @@
-function videos(e){
+function vmodal(e){
 var video = ["/",
 "https://drive.google.com/file/d/1qknvYXRYWFUQ9Xr6qYw3JtEgcGiT1pY2/preview"];
 
-Swal.fire({
-title: '<strong>Cómo conservar la comida por más tiempo</strong>', 
-html:  '¿Te gustó?</br>¡compártelo con tus amigos y familiares!' +
-    '</br></br><div class="iframe-container"><iframe title="vimeo-player" src=' + video[e] + ' width="100%" height="480" frameborder="0" allowfullscreen></iframe></div></br><div id="adsContenedor"></div></br>',
-showCloseButton: true,
-showConfirmButton: false,
-focusCancel: false,
-focusConfirm: false,
-allowOutsideClick: false
-})
+  var titulomodal = document.getElementById("titulomodal");
+  titulomodal.innerHTML = "Cómo conservar la comida por más tiempo";
 
+  var cuerpomodal = document.getElementById("cuerpomodal");
+  cuerpomodal.innerHTML = "<div class='iframe-container'><iframe src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><div id='adsContenedor'></div></br>";
 }
