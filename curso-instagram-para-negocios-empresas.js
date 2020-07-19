acbbbc0c0223f1194f961bb30dae81cd4db9267c
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-18 21:41:15 -0600

    33
    33

--- curso-instagram-para-negocios-empresas.js
+++ curso-instagram-para-negocios-empresas.js
@@ -2,33 +2,6 @@ function videos(e){
 var video = ["/",
 "https://www.youtube.com/embed/aLyWMVwnSJc?controls=0"];
 
-function ads(){
-
-var externalScript   = document.createElement("script");
-externalScript.type  = "text/javascript";
-externalScript.setAttribute('async',"");
-externalScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-document.getElementById('adsContenedor').appendChild(externalScript);
-
-var comment = document.createComment(" infocursos_adap_jul ");
-document.getElementById('adsContenedor').appendChild(comment);
-
-var ins   = document.createElement("ins");
-ins.setAttribute('class','adsbygoogle');
-ins.setAttribute('style','display:block');
-ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
-ins.setAttribute('data-ad-slot','9303445348');
-ins.setAttribute('data-ad-format','auto');
-ins.setAttribute('data-full-width-responsive','true');
-document.getElementById('adsContenedor').appendChild(ins);
-
-var inlineScript   = document.createElement("script");
-inlineScript.type  = "text/javascript";
-inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
-document.getElementById('adsContenedor').appendChild(inlineScript); 
-
-}
-
 Swal.fire({
 title: '<strong>Curso Instagram GRATIS para negocios y empresas</strong>', 
 html:  '¿Te gustó?</br>¡compártelo con tus amigos y familiares!' +
@@ -39,5 +12,5 @@ focusCancel: false,
 focusConfirm: false,
 allowOutsideClick: false
 })
-ads();
+
 }
