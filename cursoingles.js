e49c30306eb0f4d10721f65f51d207388b7c754c
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-19 07:02:02 -0600

    11
    11

--- cursoingles.js
+++ cursoingles.js
@@ -1,5 +1,5 @@
-function lecciones(e){
-var leccion = ["/",
+function vmodal(e){
+var video = ["/",
 "https://player.vimeo.com/video/392892134?title=0&byline=0&portrait=0&sidedock=0",
 "https://player.vimeo.com/video/392893056?title=0&byline=0&portrait=0&sidedock=0",
 "https://player.vimeo.com/video/392898626?title=0&byline=0&portrait=0&sidedock=0",
@@ -11,15 +11,9 @@ var leccion = ["/",
 "https://player.vimeo.com/video/392907832?title=0&byline=0&portrait=0&sidedock=0",
 "https://player.vimeo.com/video/392909060?title=0&byline=0&portrait=0&sidedock=0"];
 
-Swal.fire({
-title: '<strong>Leccion ' + e + '</strong>', 
-html:  'Disfruta nuestro fantástico curso en vídeo completamente gratis (nivel básico). Y si te gusta, ¡compártelo con tus amigos en las redes sociales!' +
-    '</br></br><div class="iframe-container"><iframe title="vimeo-player" src=' + leccion[e] + ' width="100%" height="420" frameborder="0" allowfullscreen></iframe></div></br><div id="adsContenedor"></div></br>',
-showCloseButton: true,
-showConfirmButton: false,
-focusCancel: false,
-focusConfirm: false,
-allowOutsideClick: false
-})
+  var titulomodal = document.getElementById("titulomodal");
+  titulomodal.innerHTML = "Leccion ' + e + '";
 
+  var cuerpomodal = document.getElementById("cuerpomodal");
+  cuerpomodal.innerHTML = "<div class='iframe-container'><iframe src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><div id='adsContenedor'></div></br>";
 }
