8a7cbd2b22bd1a70271d8d6ab514c21bd559c57d
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-19 19:41:48 -0600

    11
    11

--- curso-electronica.js
+++ curso-electronica.js
@@ -19,7 +19,7 @@ var video = ["/",
 "https://drive.google.com/file/d/1WWPXTtGZg5r-fLOhQVILQMpHuyfcRmUr/preview"];
 
 var titulomodal = document.getElementById("titulomodal");
-  titulomodal.innerHTML = "Curso Electrónica Básica Gratis</br>Lección " + [e];
+  titulomodal.innerHTML = "CURSO ELECTRONICA BASICA GRATIS</br>LECCIÓN " + [e];
 
 var cuerpomodal = document.getElementById("cuerpomodal");
   cuerpomodal.innerHTML = "<div class='iframe-container'><iframe src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><div id='adsContenedor'></div></br>";
