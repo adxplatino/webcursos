0ac5454bcd5bca76240934958201fbefcfee88fc
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-19 20:38:02 -0600

    1
    1

--- curso-instagram-para-negocios-empresas.js
+++ curso-instagram-para-negocios-empresas.js
@@ -1,3 +1,22 @@
+$(document).ready(function(){
+    /* Get iframe src attribute value i.e. YouTube video url
+    and store it in a variable */
+    var url = $("#modalVideo").attr('src');
+    
+    /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+    $("#mimodal").on('hide.bs.modal', function(){
+        $("#modalVideo").attr('src', '');
+    });
+    
+    /* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed again */
+    $("#mimodal").on('show.bs.modal', function(){
+        $("#modalVideo").attr('src', url);
+    });
+});
+
+
 function vmodal(e){
 var video = ["/",
 "https://www.youtube.com/embed/aLyWMVwnSJc?controls=0"];
@@ -6,5 +25,5 @@ var video = ["/",
   titulomodal.innerHTML = "Curso Instagram GRATIS";
 
   var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div class='iframe-container'><iframe src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><div id='adsContenedor'></div></br>";
+  cuerpomodal.innerHTML = "<div id='modalVideo' class='iframe-container'><iframe src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><div id='adsContenedor'></div></br>";
 }
