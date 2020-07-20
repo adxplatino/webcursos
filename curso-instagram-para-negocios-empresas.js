0a85dd208f091e00159844b812e02dbd2cf910bf
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-19 20:49:17 -0600

    11
    11

--- curso-instagram-para-negocios-empresas.js
+++ curso-instagram-para-negocios-empresas.js
@@ -1,18 +1,6 @@
 $(document).ready(function(){
-    /* Get iframe src attribute value i.e. YouTube video url
-    and store it in a variable */
-    var url = $("#modalVideo").attr('src');
-    
-    /* Assign empty url value to the iframe src attribute when
-    modal hide, which stop the video playing */
     $("#mimodal").on('hide.bs.modal', function(){
         $("#modalVideo").attr('src', '');
-    });
-    
-    /* Assign the initially stored url back to the iframe src
-    attribute when modal is displayed again */
-    $("#mimodal").on('show.bs.modal', function(){
-        $("#modalVideo").attr('src', url);
     });
 });
 
