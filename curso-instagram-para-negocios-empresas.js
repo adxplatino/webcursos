09e8966f55352679abf753aa049dfd298c7dea28
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-19 20:56:08 -0600

    11
    11

--- curso-instagram-para-negocios-empresas.js
+++ curso-instagram-para-negocios-empresas.js
@@ -12,6 +12,5 @@ var video = ["/",
   var titulomodal = document.getElementById("titulomodal");
   titulomodal.innerHTML = "Curso Instagram GRATIS";
 
-  var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div id='modalVideo' class='iframe-container'><iframe src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><div id='adsContenedor'></div></br>";
+ 
 }
