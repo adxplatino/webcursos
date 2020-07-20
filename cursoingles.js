c959ae5ca53063745b0482d805bf6353c6b74119
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-19 18:22:59 -0600

    11
    11

--- cursoingles.js
+++ cursoingles.js
@@ -10,10 +10,8 @@ var video = ["/",
 "https://player.vimeo.com/video/392906674?title=0&byline=0&portrait=0&sidedock=0",
 "https://player.vimeo.com/video/392907832?title=0&byline=0&portrait=0&sidedock=0",
 "https://player.vimeo.com/video/392909060?title=0&byline=0&portrait=0&sidedock=0"];
-
   var titulomodal = document.getElementById("titulomodal");
-  titulomodal.innerHTML = "Curso De Inglés GRATIS";
-
+  titulomodal.innerHTML = "Curso De Inglés GRATIS</br>Lección "[e];
   var cuerpomodal = document.getElementById("cuerpomodal");
   cuerpomodal.innerHTML = "<div class='iframe-container'><iframe src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><div id='adsContenedor'></div></br>";
 }
