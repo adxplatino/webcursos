f524d0a2ea46d9988d1e2e47df0d422e84bdcd49
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-25 07:21:27 -0600

    11
    11

--- cursoingles.js
+++ cursoingles.js
@@ -42,8 +42,9 @@ var titulomodal = document.getElementById("titulomodal");
   titulomodal.innerHTML = "CURSO DE INGLES GRATIS</br>LECCIÓN " + [e];
 
 var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div class='iframe-container'><iframe id='modalVideo' src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><div id='adsContenedor'></div></br>";
-ads();
+  cuerpomodal.innerHTML = "<div class='iframe-container'><iframe id='modalVideo' src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><center><div id='adsContenedor'></div></center></br>";
+
+ ads();
 }
 function cerrarmodal(){
   $("#modalVideo").attr('src', '');
