222b77dcadf1aebeb0a668281ce7abddfe37ceb6
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-25 06:23:56 -0600

    11
    11

--- cursoingles.js
+++ cursoingles.js
@@ -10,12 +10,40 @@ var video = ["/",
 "https://player.vimeo.com/video/392906674?title=0&byline=0&portrait=0&sidedock=0",
 "https://player.vimeo.com/video/392907832?title=0&byline=0&portrait=0&sidedock=0",
 "https://player.vimeo.com/video/392909060?title=0&byline=0&portrait=0&sidedock=0"];
+  
+function ads(){
+
+var externalScript   = document.createElement("script");
+externalScript.type  = "text/javascript";
+externalScript.setAttribute('async',"");
+externalScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+document.getElementById('adsContenedor').appendChild(externalScript);
+
+var comment = document.createComment(" infocursos_adap_jul ");
+document.getElementById('adsContenedor').appendChild(comment);
+
+var ins   = document.createElement("ins");
+ins.setAttribute('class','adsbygoogle');
+ins.setAttribute('style','display:block');
+ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
+ins.setAttribute('data-ad-slot','9303445348');
+ins.setAttribute('data-ad-format','auto');
+ins.setAttribute('data-full-width-responsive','true');
+document.getElementById('adsContenedor').appendChild(ins);
+
+var inlineScript   = document.createElement("script");
+inlineScript.type  = "text/javascript";
+inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
+document.getElementById('adsContenedor').appendChild(inlineScript); 
+
+}
 
 var titulomodal = document.getElementById("titulomodal");
   titulomodal.innerHTML = "CURSO DE INGLES GRATIS</br>LECCIÓN " + [e];
 
 var cuerpomodal = document.getElementById("cuerpomodal");
   cuerpomodal.innerHTML = "<div class='iframe-container'><iframe id='modalVideo' src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><div id='adsContenedor'></div></br>";
+ads();
 }
 function cerrarmodal(){
   $("#modalVideo").attr('src', '');
