076253d1b77669c9a2f1ba11dc43c818823b165c
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-26 06:15:28 -0600

    11
    11

--- curso-electronica.js
+++ curso-electronica.js
@@ -23,19 +23,17 @@ function ads(){
 var externalScript   = document.createElement("script");
 externalScript.type  = "text/javascript";
 externalScript.setAttribute('async',"");
-externalScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+externalScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
 document.getElementById('adsContenedor').appendChild(externalScript);
 
-var comment = document.createComment(" infocursos_adap_jul ");
+var comment = document.createComment(" infocursos-300x250 ");
 document.getElementById('adsContenedor').appendChild(comment);
 
 var ins   = document.createElement("ins");
 ins.setAttribute('class','adsbygoogle');
-ins.setAttribute('style','display:block');
 ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
-ins.setAttribute('data-ad-slot','9303445348');
-ins.setAttribute('data-ad-format','auto');
-ins.setAttribute('data-full-width-responsive','true');
+ins.setAttribute('data-ad-slot','9029296375');
+ins.setAttribute('style','display:inline-block;width:300px;height:250px');
 document.getElementById('adsContenedor').appendChild(ins);
 
 var inlineScript   = document.createElement("script");
@@ -51,7 +49,7 @@ var titulomodal = document.getElementById("titulomodal");
 var cuerpomodal = document.getElementById("cuerpomodal");
   cuerpomodal.innerHTML = "<div class='iframe-container'><iframe id='modalVideo' src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><center><div id='adsContenedor'></div></center></br>";
 
-  ads();
+ads();
 }
 function cerrarmodal(){
   $("#modalVideo").attr('src', '');
