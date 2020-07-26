9927261d1dfaf7ab7b9a551af473f4b1d5584a56
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-26 06:00:43 -0600

    11
    11

--- cursoingles.js
+++ cursoingles.js
@@ -11,7 +11,7 @@ var video = ["/",
 "https://player.vimeo.com/video/392907832?title=0&byline=0&portrait=0&sidedock=0",
 "https://player.vimeo.com/video/392909060?title=0&byline=0&portrait=0&sidedock=0"];
   
-function ads(){
+ function ads(){
 
 var externalScript   = document.createElement("script");
 externalScript.type  = "text/javascript";
@@ -19,16 +19,14 @@ externalScript.setAttribute('async',"");
 externalScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
 document.getElementById('adsContenedor').appendChild(externalScript);
 
-var comment = document.createComment(" infocursos_adap_jul ");
+var comment = document.createComment(" infocursos-300x250 ");
 document.getElementById('adsContenedor').appendChild(comment);
 
 var ins   = document.createElement("ins");
 ins.setAttribute('class','adsbygoogle');
-ins.setAttribute('style','display:block');
 ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
-ins.setAttribute('data-ad-slot','9303445348');
-ins.setAttribute('data-ad-format','auto');
-ins.setAttribute('data-full-width-responsive','true');
+ins.setAttribute('data-ad-slot','9029296375');
+ins.setAttribute('style','display:inline-block;width:300px;height:250px');
 document.getElementById('adsContenedor').appendChild(ins);
 
 var inlineScript   = document.createElement("script");
