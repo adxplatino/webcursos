fd839680e5e4b6aacc4e4467e6bd4f6672a5949e
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-07-31 14:33:07 -0600

    11
    11

--- curso-instagram-para-negocios-empresas.js
+++ curso-instagram-para-negocios-empresas.js
@@ -3,23 +3,23 @@ function vmodal(e){
 function ads(){
 
 var externalScript   = document.createElement("script");
+externalScript.type  = "text/javascript";
 externalScript.setAttribute('async',"");
-externalScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+externalScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
 document.getElementById('adsContenedor').appendChild(externalScript);
 
-var comment = document.createComment(" infocursos_adap_jul ");
+var comment = document.createComment(" infocursos-300x250 ");
 document.getElementById('adsContenedor').appendChild(comment);
 
 var ins   = document.createElement("ins");
 ins.setAttribute('class','adsbygoogle');
-ins.setAttribute('style','display:block');
 ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
-ins.setAttribute('data-ad-slot','9303445348');
-ins.setAttribute('data-ad-format','auto');
-ins.setAttribute('data-full-width-responsive','true');
+ins.setAttribute('data-ad-slot','9029296375');
+ins.setAttribute('style','display:inline-block;width:300px;height:250px');
 document.getElementById('adsContenedor').appendChild(ins);
 
 var inlineScript   = document.createElement("script");
+inlineScript.type  = "text/javascript";
 inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
 document.getElementById('adsContenedor').appendChild(inlineScript); 
 
