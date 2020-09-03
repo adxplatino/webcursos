5731f23dfe75c94cccd43b4a7068846ed8f187f6
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-09-03 12:09:40 -0600

    22
    22

--- curso-carpinteria.js
+++ curso-carpinteria.js
@@ -64,7 +64,7 @@ titulomodal.innerHTML = "CURSO DE CARPINTERIA GRATIS</br>LECCIÓN " + [e];
 
 var cuerpomodal = document.getElementById("cuerpomodal");
   cuerpomodal.innerHTML = "<div class='iframe-container'><iframe id='modalVideo' src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><center><div id='adsContenedor'></div></center></br>";
-
+ads();
 }
 function cerrarmodal(){
   $("#modalVideo").attr('src', '');
