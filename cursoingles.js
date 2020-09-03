6f4b907bd1c788b35de57d27b50d1e4dc991eaa1
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-09-03 11:54:01 -0600

    11
    11

--- cursoingles.js
+++ cursoingles.js
@@ -10,7 +10,32 @@ var video = ["/",
 "https://player.vimeo.com/video/392906674?title=0&byline=0&portrait=0&sidedock=0",
 "https://player.vimeo.com/video/392907832?title=0&byline=0&portrait=0&sidedock=0",
 "https://player.vimeo.com/video/392909060?title=0&byline=0&portrait=0&sidedock=0"];
-  
+
+function ads(){
+
+var externalScript   = document.createElement("script");
+externalScript.type  = "text/javascript";
+externalScript.setAttribute('async',"");
+externalScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+document.getElementById('adsContenedor').appendChild(externalScript);
+
+var comment = document.createComment(" infocursos-300x250 ");
+document.getElementById('adsContenedor').appendChild(comment);
+
+var ins   = document.createElement("ins");
+ins.setAttribute('class','adsbygoogle');
+ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
+ins.setAttribute('data-ad-slot','9029296375');
+ins.setAttribute('style','display:inline-block;width:300px;height:250px');
+document.getElementById('adsContenedor').appendChild(ins);
+
+var inlineScript   = document.createElement("script");
+inlineScript.type  = "text/javascript";
+inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
+document.getElementById('adsContenedor').appendChild(inlineScript); 
+
+}
+   
 var titulomodal = document.getElementById("titulomodal");
   titulomodal.innerHTML = "CURSO DE INGLES GRATIS</br>LECCIÓN " + [e];
 
