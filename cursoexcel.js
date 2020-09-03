5a98c92555a3a2eb02820a11639a04d52af06a8c
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-09-03 12:08:04 -0600

    22
    22

--- cursoexcel.js
+++ cursoexcel.js
@@ -87,7 +87,7 @@ var titulomodal = document.getElementById("titulomodal");
 
 var cuerpomodal = document.getElementById("cuerpomodal");
   cuerpomodal.innerHTML = "<div class='iframe-container'><iframe id='modalVideo' src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><center><div id='adsContenedor'></div></center></br>";
-
+ads();
 }
 function cerrarmodal(){
   $("#modalVideo").attr('src', '');
