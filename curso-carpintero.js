ee399cebb991b74c762451a915604663d059db85
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-09-26 14:53:20 -0600

    22
    22

--- curso-carpintero.js
+++ curso-carpintero.js
@@ -66,7 +66,7 @@ document.getElementById('adsContenedor').appendChild(inlineScript);
 var titulomodal = document.getElementById("titulomodal");
 titulomodal.innerHTML = "CURSO DE CARPINTERIA GRATIS</br>LECCIÓN " + [e];
 
-var cuerpomodal = jwplayer("cuerpomodal").setup({ "playlist": [{ "file": "https://cdn11.capacitateparaelempleo.org/mobilified/nBbfIZ3VUFs.mp4"}]});
+var playeromodal = jwplayer("cuerpomodal").setup({ "playlist": [{ "file": "https://cdn11.capacitateparaelempleo.org/mobilified/nBbfIZ3VUFs.mp4"}]});
 
 var cuerpomodal = document.getElementById("cuerpomodal");
   cuerpomodal.innerHTML = "</br><center><div id='adsContenedor'></div></center></br>";
