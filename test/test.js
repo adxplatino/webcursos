098b5970511306496b4bc02dfccb3d3fb7e05e01
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-12-14 19:51:10 -0600

    qq
    qq

--- test/test.js
+++ test/test.js
@@ -4,13 +4,25 @@ function ads(){
 document.getElementById('adsContenedor1').innerHTML = 'ADS1!';
 document.getElementById('adsContenedor2').innerHTML = 'ADS2!';
 }
+function captcha(){
 
+var externalScript   = document.createElement("script");
+externalScript.type  = "text/javascript";
+externalScript.setAttribute('async',"");
+externalScript.src = "https://www.google.com/recaptcha/api.js?hl=es";
+document.getElementById('captchascript').appendChild(externalScript);
+  
+var inlineScript   = document.createElement("script");
+inlineScript.type  = "text/javascript";  
+document.getElementById('captchascript').appendChild(inlineScript); 
+}
 var titulomodal = document.getElementById("titulomodal");
   titulomodal.innerHTML = "Please check the captcha box to proceed to the destination page";
 
 var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <center><div id='adsContenedor1'></div></center></br> <div style='text-align: center;'><button class='btn btn-outline-primary btn-captcha m-2 disabled' id='botoncaptcha' onclick='window.open('https://www.infoproyectos.com/p/000002.html','_self')'  type='submit' disabled='disabled'>Click here to continue</button></div></br> <center><div id='adsContenedor2'></div></center></br>";
+  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <center><div id='adsContenedor1'></div></center></br> <div style='text-align: center;'><button class='btn btn-outline-primary btn-captcha m-2 disabled' id='botoncaptcha' onclick='window.open('https://www.infoproyectos.com/p/000002.html','_self')'  type='submit' disabled='disabled'>Click here to continue</button></div></br> <center><div id='adsContenedor2'></div></center></br><div id='captchascript'></div>";
 ads();
+captcha();
 }
 
 function CaptchaChecked() { 
