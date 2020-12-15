72a35c972f36677f069ab368819185d2b94f5c35
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-12-14 18:17:13 -0600

    11
    11

--- test/test.js
+++ test/test.js
@@ -1,14 +1,15 @@
 function vmodal(e){
   
 function ads(){
-document.getElementById('adsContenedor').innerHTML = 'ADS!';
+document.getElementById('adsContenedor1').innerHTML = 'ADS1!';
+document.getElementById('adsContenedor2').innerHTML = 'ADS2!';
 }
 
 var titulomodal = document.getElementById("titulomodal");
   titulomodal.innerHTML = "Please check the captcha box to proceed to the destination page";
 
 var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <center><div id='adsContenedor'></div></center></br> <div style='text-align: center;'><button class='btn btn-outline-primary btn-captcha m-2 disabled' id='botoncaptcha' onclick='window.open('https://www.infoproyectos.com/p/000002.html','_self')'  type='submit' disabled='disabled'>Click here to continue</button></div></br> <center><div id='adsContenedor'></div></center></br>";
+  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <center><div id='adsContenedor1'></div></center></br> <div style='text-align: center;'><button class='btn btn-outline-primary btn-captcha m-2 disabled' id='botoncaptcha' onclick='window.open('https://www.infoproyectos.com/p/000002.html','_self')'  type='submit' disabled='disabled'>Click here to continue</button></div></br> <center><div id='adsContenedor2'></div></center></br>";
 ads();
 }
 
