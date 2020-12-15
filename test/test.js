5a33225d7c6830f18b1977d956fe6dad7988dcdd
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-12-15 02:53:01 -0600

    ss
    ss

--- test/test.js
+++ test/test.js
@@ -16,18 +16,20 @@ var inlineScript   = document.createElement("script");
 inlineScript.type  = "text/javascript";  
 document.getElementById('captchascript').appendChild(inlineScript); 
 }
-function miurl() {
-  var abrirurl = window.open('https://www.w3schools.com','_self');
-}
+
 var titulomodal = document.getElementById("titulomodal");
   titulomodal.innerHTML = "Please check the captcha box to proceed to the destination page";
 
 var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <center><div id='adsContenedor1'></div></center></br><div style='text-align: center;'><input class='btn btn-outline-primary btn-captcha m-2 disabled' id='enlacecaptcha' disabled='disabled' type='button' value='Click here to continue' onClick='miurl();'></div></br>  <center><div id='adsContenedor2'></div></center></br><div id='captchascript'></div>";
+  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <center><div id='adsContenedor1'></div></center></br> <div style='text-align: center;'><button class='btn btn-outline-primary btn-captcha m-2 disabled' id='enlacecaptcha' disabled='disabled'>Click here to continue</button></div></br> <center><div id='adsContenedor2'></div></center></br><div id='captchascript'></div>";
 ads();
 captcha();
 }
 
-function CaptchaChecked() { 
+function CaptchaChecked() {
+  var abrirurl = document.getElementById("enlacecaptcha");
+  abrirurl.onclick = function(){
+var abrirurl = window.open('https://www.w3schools.com','_self');
+}
   jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
 }
