dc3255447e07f0e97237a915f148bea52aa7e0e1
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-12-20 07:21:01 -0600

    22
    22

--- curso-melamina/contenido.js
+++ curso-melamina/contenido.js
@@ -1,5 +1,11 @@
 function vmodal(e){
-
+  
+var leccion = ["/",              
+"/p/contenido-curso-de-melamina-herramientas.html",
+"/p/contenido-curso-de-melamina-planos.html",
+"/p/contenido-curso-de-melamina-tecnicas.html",
+"/p/contenido-curso-de-melamina-estrategia.html"];
+  
 function ads(){
 document.getElementById('adsContenedor1').innerHTML = 'ADS1!';
 document.getElementById('adsContenedor2').innerHTML = 'ADS2!';
@@ -21,22 +27,9 @@ var titulomodal = document.getElementById("titulomodal");
   titulomodal.innerHTML = "Please check the captcha box to proceed to the destination page";
 
 var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LcOpA0aAAAAAAFy1-uMmBz40geaohdAZBZvcNYs' data-callback='CaptchaChecked'></div></div></div> </br> <center><div id='adsContenedor1'></div></center></br> <div style='text-align: center;'><button class='btn btn-outline-primary btn-captcha m-2 disabled' id='enlacecaptcha' disabled='disabled'>Click here to continue</button></div></br> <center><div id='adsContenedor2'></div></center></br><div id='captchascript'></div>";
+  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LcOpA0aAAAAAAFy1-uMmBz40geaohdAZBZvcNYs' data-callback='CaptchaChecked'></div></div></div> </br> <center><div id='adsContenedor1'></div></center></br> <div style='text-align: center;'><button class='btn btn-outline-primary btn-captcha m-2 disabled' id='enlacecaptcha' disabled='disabled' onclick='window.open('+ leccion[e] +')'>Click here to continue</button></div></br> <center><div id='adsContenedor2'></div></center></br><div id='captchascript'></div>";
 ads();
 captcha();
-}
-
-function CaptchaChecked() {
-var abrirurl = document.getElementById("enlacecaptcha");
-abrirurl.onclick = function(){
-  
-var leccion = ["/",              
-"/p/contenido-curso-de-melamina-herramientas.html",
-"/p/contenido-curso-de-melamina-planos.html",
-"/p/contenido-curso-de-melamina-tecnicas.html",
-"/p/contenido-curso-de-melamina-estrategia.html"];
-  
-var abrirurl = window.open(leccion[e]);
 }
 jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
 }
