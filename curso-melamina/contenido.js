12f3d4b6d729815eb62964501f00ea311c1736c0
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-12-20 07:05:40 -0600

    11
    11

--- curso-melamina/contenido.js
+++ curso-melamina/contenido.js
@@ -36,7 +36,7 @@ var leccion = ["/",
 "/p/contenido-curso-de-melamina-tecnicas.html",
 "/p/contenido-curso-de-melamina-estrategia.html"];
   
-var abrirurl = window.open'leccion[e]';
+var abrirurl = window.open(leccion[e]);
 }
 jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
 }
