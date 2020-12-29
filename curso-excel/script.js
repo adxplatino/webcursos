7de57cd1cd1f47993b5a6f85824d5ffca76c82b9
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-12-28 18:19:47 -0600

    11
    11

---
+++ curso-excel/script.js
@@ -0,0 +1,89 @@
+function vmodal(e){
+var video = ["/",
+"https://drive.google.com/file/d/1bFOpc6dLzaIqNIIb8ke8SXIR0_RpC91l/preview",
+"https://drive.google.com/file/d/1_nS9NFiesso7uOnPCiZlM_EXss-p7hxx/preview",
+"https://drive.google.com/file/d/1mx25EAHAY8m6qv0PLdi3pXWQlA3XU4P3/preview",
+"https://drive.google.com/file/d/1guYT7_HN6A1wJqXTY-nGepops1_r7gli/preview",
+"https://drive.google.com/file/d/1yOotv2R87iV98oICgxpylj5j6UR8EIjk/preview",
+"https://drive.google.com/file/d/180ek83MPSc0LEKx056-sbUoPvyJNlfo7/preview",
+"https://drive.google.com/file/d/1Kh46A7GxwOjnxge3t6V2XZDQBOmrxLaJ/preview",
+
+"https://drive.google.com/file/d/1K2VtcviMA4wobjdKV4Zd4VCLOfyJwxLo/preview",
+"https://drive.google.com/file/d/1Ktk1p3UugX2GiWJ0XxgOyZSkWWrbVyON/preview",
+"https://drive.google.com/file/d/1p_L0311crnb15CwDrIO8ZuNLJJySLIYi/preview",
+"https://drive.google.com/file/d/1kFjhdHjSsreopma3BW6YFfN-RhZNS4wb/preview",
+"https://drive.google.com/file/d/1_CchNB8edy3lBP33lqr6CxUMNij9xoJj/preview",
+"https://drive.google.com/file/d/1yVxXQufP9MMQ42hGziE9scLxhkMO___Y/preview",
+"https://drive.google.com/file/d/1B3W2z4Df1bpfKZs95kaWBpiA6CUz1Iw_/preview",
+"https://drive.google.com/file/d/1wfHOzJ5h8mYDQh-3_rPXna5bXnkZe6b1/preview",
+"https://drive.google.com/file/d/1OlSgB0a-0HYn-IkyHpKLTg5aQdJsfhNN/preview",
+
+"https://drive.google.com/file/d/1f-v1VwpFnL8Ps9qEzKZ6_TtkthZlxI95/preview",
+"https://drive.google.com/file/d/1f6dIgwOoPYbhvgfetVYbzTI6Nki708nL/preview",
+
+"https://drive.google.com/file/d/1KqJz_-gKJLO4y4PMjtirxAhEbi1nI97A/preview",
+"https://drive.google.com/file/d/1kXHXkh441vGd1PkkqMNp0nulbjcFN16-/preview",
+
+"https://drive.google.com/file/d/19RK4kwJxctgvtd4wvH4wtD6JBnfMfPcU/preview",
+"https://drive.google.com/file/d/1_vQPVvbpUIophd03w9cq2Yqw6ni51rQr/preview",
+"https://drive.google.com/file/d/1PeiSewNc1BlJXTLg2lXbc2gIMIxTUleR/preview",
+"https://drive.google.com/file/d/1Y8nw2eApos1gYpiXzpSDliyHPNsaYSWJ/preview",
+
+"https://drive.google.com/file/d/1Vo23W2aHii_ELYshY0A04w3KRvAAlO9h/preview",
+"https://drive.google.com/file/d/1EhhaeaWcf8gF3AhxDhVUA9u85mV6zHLc/preview",
+"https://drive.google.com/file/d/13wS_bfLKZ-5OCSRA1Fsp7tITHi-YX-pz/preview",
+"https://drive.google.com/file/d/1XaP4xOCWfRiQR5HmZ9Dyu3q_fO0AkH4x/preview",
+"https://drive.google.com/file/d/1ypl8Y-xt8JvAH2-Vs1lSxEE74UK8ba8F/preview",
+
+"https://drive.google.com/file/d/18KlPbFDg-GcV6-NsAXL_iLkMEtTOE_iV/preview",
+"https://drive.google.com/file/d/1EK-2ndiD4MPQjSl7t15oG3ROnV2pamFb/preview",
+"https://drive.google.com/file/d/13k5I2VIqT0fmG1wvRk2UXDxaEgrm-H_U/preview",
+"https://drive.google.com/file/d/1853kOeiTW0Ut_DiCeJ64EXVsbFBk0RiE/preview",
+
+"https://drive.google.com/file/d/1LJ97zFu6Pr81kia_xOuZyoIjWcNvUTz9/preview",
+"https://drive.google.com/file/d/1H1rYzO_yB6aBmEAWdghrU_Ec5FUETX3z/preview",
+"https://drive.google.com/file/d/1nzW21gU5JSOsssdQ0B1qLgmMR17DNNDH/preview",
+"https://drive.google.com/file/d/1KSaICsFccpdSvbirwrh_sq3t7n01pfFd/preview",
+"https://drive.google.com/file/d/1l4TcyA1TMvHEooog2E8VuOnzwa48yGOP/preview",
+
+"https://drive.google.com/file/d/1PNbXs1SRYv3GJkk9-NA6aCRJP5xArP_R/preview",
+"https://drive.google.com/file/d/1NYdnTCR2AP1cWWMhjeGxA5k0F2hMVrj8/preview",
+
+"https://drive.google.com/file/d/1rmK9i_fWhEPqr-EZmDUbgXWp6Lp8D-ki/preview",
+"https://drive.google.com/file/d/1r9cW_7neXIUdNOZcgkTXhNEyemyaFZ-L/preview",
+"https://drive.google.com/file/d/1rZdBPowyTsQvEMSdaFGR-e3oDExSebow/preview",
+"https://drive.google.com/file/d/1ogv3Kq8ORA7BEK4Cngkq_vEsxFw0fvg9/preview",
+"https://drive.google.com/file/d/1DtqyP3mqyTu5bjVuqFyYrPJJNOAHdt2r/preview",
+"https://drive.google.com/file/d/1OUNEx6R_PId86XrPUWqwMuyW9xlKS1GF/preview",
+"https://drive.google.com/file/d/153LtCVSZDwKZ2bMBw-P1efkzqbxfzj2Z/preview"];
+ 
+function ads(){
+var externalScript   = document.createElement("script");
+externalScript.type  = "text/javascript";
+externalScript.setAttribute('async',"");
+externalScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+document.getElementById('adsContenedor').appendChild(externalScript);
+var comment = document.createComment(" infocursos-300x250 ");
+document.getElementById('adsContenedor').appendChild(comment);
+var ins   = document.createElement("ins");
+ins.setAttribute('class','adsbygoogle');
+ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
+ins.setAttribute('data-ad-slot','9029296375');
+ins.setAttribute('style','display:inline-block;width:300px;height:250px');
+document.getElementById('adsContenedor').appendChild(ins);
+var inlineScript   = document.createElement("script");
+inlineScript.type  = "text/javascript";
+inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
+document.getElementById('adsContenedor').appendChild(inlineScript); 
+}
+ 
+var titulomodal = document.getElementById("titulomodal");
+  titulomodal.innerHTML = "CURSO EXCEL</br>LECCIÓN " + [e];
+
+var cuerpomodal = document.getElementById("cuerpomodal");
+  cuerpomodal.innerHTML = "<div class='iframe-container'><iframe id='modalVideo' src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><center><div id='adsContenedor'></div></center></br>";
+ads();
+}
+function cerrarmodal(){
+  $("#modalVideo").attr('src', '');
+}
