1ddc7ab842dedc21aee10674e1d022654028f9aa
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-12-28 21:11:47 -0600

    11
    11

--- curso-muebles-melamina/contenido.js
+++ curso-muebles-melamina/contenido.js
@@ -41,7 +41,7 @@ var titulomodal = document.getElementById("titulomodal");
   titulomodal.innerHTML = "Please check the captcha box to proceed to the destination page";
 
 var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <center><div id='adsContenedor'></div></center></br> <div style='text-align: center;'><a href="+leccion[e]+" class='btn btn-outline-primary btn-captcha m-2 disabled' id='enlacecaptcha' disabled='disabled'>Click here to continue</a></div></br><div id='captchascript'></div>";
+  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <center><div id='adsContenedor'></div></center></br> <div style='text-align: center;'><a href="+leccion[e]+" class='btn btn-outline-primary btn-captcha m-2 disabled' id='enlacecaptcha' disabled='disabled'>Continuar →</a></div></br><div id='captchascript'></div>";
 ads();
 captcha();
 }
