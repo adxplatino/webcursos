cba803befef4fe04d390cf1f45cfc99885316f5f
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2021-01-20 12:51:17 -0600

    1
    1

--- curso-carpintero.js
+++ curso-carpintero.js
@@ -37,32 +37,7 @@ var video = ["/",
 "https://cdn11.capacitateparaelempleo.org/mobilified/DRGweoyYjEg.mp4",
 "https://cdn11.capacitateparaelempleo.org/mobilified/X24OhGsmleg.mp4",
 "https://cdn11.capacitateparaelempleo.org/mobilified/nBbfIZ3VUFs.mp4"];
-
-function ads(){
-
-var externalScript   = document.createElement("script");
-externalScript.type  = "text/javascript";
-externalScript.setAttribute('async',"");
-externalScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-document.getElementById('adsContenedor').appendChild(externalScript);
-
-var comment = document.createComment(" infocursos-300x250 ");
-document.getElementById('adsContenedor').appendChild(comment);
-
-var ins   = document.createElement("ins");
-ins.setAttribute('class','adsbygoogle');
-ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
-ins.setAttribute('data-ad-slot','9029296375');
-ins.setAttribute('style','display:inline-block;width:300px;height:250px');
-document.getElementById('adsContenedor').appendChild(ins);
-
-var inlineScript   = document.createElement("script");
-inlineScript.type  = "text/javascript";
-inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
-document.getElementById('adsContenedor').appendChild(inlineScript); 
-
-}
-    
+  
     
 var titulomodal = document.getElementById("titulomodal");
 titulomodal.innerHTML = "CURSO DE CARPINTERIA GRATIS</br>LECCIÓN " + [e];
@@ -71,8 +46,6 @@ var player = jwplayer('cuerpomodal').setup({file: video[e] });
 
 var adsmodal = document.getElementById("adsmodal");
   adsmodal.innerHTML = "<br /><center><div id='adsContenedor'></div></center><br />";
-    
-ads();
 }
 function cerrarmodal(){
   $(".jw-video").attr('src', '');
