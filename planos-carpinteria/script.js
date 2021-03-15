905ee9dca74749bca658a41a4d33cfb99fc4573d
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2021-03-15 08:09:43 -0600

    11
    11

--- planos-carpinteria/script.js
+++ planos-carpinteria/script.js
@@ -33,7 +33,7 @@ document.getElementById('captchascript').appendChild(inlineScript);
 }
 
 var titulomodal = document.getElementById("titulomodal");
-  titulomodal.innerHTML = "Please check the captcha box to proceed to the destination page";
+  titulomodal.innerHTML = "Please check the captcha box to proceed to the destination page PDF";
 
 var cuerpomodal = document.getElementById("cuerpomodal");
   cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <p style='text-align: center'>Planos Gratis para Fabricar Muebles de Melamina</p> <center><div id='adsContenedor'></div></center></br> <div style='text-align: center;'><button class='btn btn-outline-primary btn-captcha m-2 disabled' id='enlacecaptcha' disabled='disabled'>Continuar →</button></div></br><div id='captchascript'></div>";
@@ -57,7 +57,7 @@ var linkplano = ["/",
  
 
  
-var abrirurl = window.open('linkplano[e]','_self');
+var abrirurl = window.open(linkplano[e],'_self');
 }
 jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
 }
