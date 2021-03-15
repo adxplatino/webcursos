2b2972dde493a2a14f69cd89bf4623032052602f
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2021-03-15 10:20:54 -0600

    11
    11

--- curso-tatuajes/script.js
+++ curso-tatuajes/script.js
@@ -47,7 +47,7 @@ var titulomodal = document.getElementById("titulomodal");
 titulomodal.innerHTML = "CURSO DE TATUAJES</br>LECCIÓN " + [e];
 
 var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div class='iframe-container'><iframe id='modalVideo' src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br><center><div id='adsContenedor'></div></center></br>";
+  cuerpomodal.innerHTML = "<div class='iframe-container'><iframe id='modalVideo' src="+video[e]+" width='100%' height='480' frameborder='0' allowfullscreen></iframe></div></br> <p style='text-align: center'>Curso gratis de Iniciación al Tatuaje.</p> <center><div id='adsContenedor'></div></center></br>";
 ads();
 }
 function cerrarmodal(){
