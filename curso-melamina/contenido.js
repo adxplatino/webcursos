7f874fc87e3503d85f8d5508b036b5256f303686
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2021-03-22 08:38:30 -0600

    11
    11

--- curso-melamina/contenido.js
+++ curso-melamina/contenido.js
@@ -5,32 +5,7 @@ var leccion = ["/",
 "/p/contenido-curso-de-melamina-planos.html",
 "/p/contenido-curso-de-melamina-tecnicas.html",
 "/p/contenido-curso-de-melamina-estrategia.html"];
-  
-function ads(){
-  
-var externalScript   = document.createElement("script");
-externalScript.type  = "text/javascript";
-externalScript.setAttribute('async',"");
-externalScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-document.getElementById('adsContenedor').appendChild(externalScript);
 
-var comment = document.createComment(" infocursos-300x250 ");
-document.getElementById('adsContenedor').appendChild(comment);
-
-var ins   = document.createElement("ins");
-ins.setAttribute('class','adsbygoogle');
-ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
-ins.setAttribute('data-ad-slot','9029296375');
-ins.setAttribute('style','display:inline-block;width:300px;height:250px');
-document.getElementById('adsContenedor').appendChild(ins);
-
-var inlineScript   = document.createElement("script");
-inlineScript.type  = "text/javascript";
-inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
-document.getElementById('adsContenedor').appendChild(inlineScript);
-  
-}
-  
 function captcha(){
 
 var externalScript   = document.createElement("script");
@@ -49,7 +24,6 @@ var titulomodal = document.getElementById("titulomodal");
 
 var cuerpomodal = document.getElementById("cuerpomodal");
   cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LcOpA0aAAAAAAFy1-uMmBz40geaohdAZBZvcNYs' data-callback='CaptchaChecked'></div></div></div> </br> <p style='text-align: center'>VER VIDEO LECCIÓN - ESTRATEGIA SECRETA - Curso Fabricar y Vender Muebles de Melamina Desde Casa.</p> <center><div id='adsContenedor'></div></center></br> <div style='text-align: center;'><a href="+leccion[e]+" class='btn btn-outline-primary btn-captcha m-2 disabled' id='enlacecaptcha' disabled='disabled'>Click here to continue</a></div></br><div id='captchascript'></div>";
-ads();
 captcha();
 }
 function CaptchaChecked() {
