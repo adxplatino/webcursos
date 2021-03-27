ad7f47ed88d203d2db24a334b74e39c57dc06fa0
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2021-03-27 17:03:45 -0600

    11
    11

--- script.js
+++ script.js
@@ -24,7 +24,28 @@ document.getElementById('adxarticle').appendChild(inlineScript);
 }());   
     
     
-    
+(function () {
+var externalScript   = document.createElement("script");
+externalScript.type  = "text/javascript";
+externalScript.setAttribute('async',"");
+externalScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+document.getElementById('adx336x280').appendChild(externalScript);
+
+var comment = document.createComment(" infoadxbody-336x280 ");
+document.getElementById('adx336x280').appendChild(comment);
+
+var ins   = document.createElement("ins");
+ins.setAttribute('class','adsbygoogle');
+ins.setAttribute('style','display:inline-block;width:336px;height:280px');
+ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
+ins.setAttribute('data-ad-slot','9490809578');
+document.getElementById('adx336x280').appendChild(ins);
+
+var inlineScript   = document.createElement("script");
+inlineScript.type  = "text/javascript";
+inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
+document.getElementById('adx336x280').appendChild(inlineScript);
+}());     
 
     
 }
