049f33d2288488dc7aa106935e08f017cf957caf
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2021-03-27 16:52:47 -0600

    11
    11

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 function adx(){
 
     
- (function () {
+(function () {
 var externalScript   = document.createElement("script");
 externalScript.type  = "text/javascript";
 externalScript.setAttribute('async',"");
 externalScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-document.getElementById('adx300').appendChild(externalScript);
-
-var comment = document.createComment(" infocursos-300x250 ");
-document.getElementById('adx300').appendChild(comment);
+document.getElementById('adxarticle').appendChild(externalScript);
 
 var ins   = document.createElement("ins");
 ins.setAttribute('class','adsbygoogle');
 ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
-ins.setAttribute('data-ad-slot','9029296375');
-ins.setAttribute('style','display:inline-block;width:300px;height:250px');
-document.getElementById('adx300').appendChild(ins);
+ins.setAttribute('data-ad-format','fluid');
+ins.setAttribute('data-ad-layout','in-article');
+ins.setAttribute('data-ad-slot','3832353166');
+ins.setAttribute('style','display: block; text-align: center;');
+document.getElementById('adxarticle').appendChild(ins);
 
 var inlineScript   = document.createElement("script");
 inlineScript.type  = "text/javascript";
 inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
-document.getElementById('adx300').appendChild(inlineScript);
+document.getElementById('adxarticle').appendChild(inlineScript);
 }());   
     
     
