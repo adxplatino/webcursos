730ba8efd8e421dc6df496f68c39eb826697e5ad
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2021-03-27 17:34:26 -0600

    11
    11

--- script.js
+++ script.js
@@ -24,30 +24,7 @@ document.getElementById('adxarticle').appendChild(inlineScript);
 }());
     
     
-(function () {
-var externalScript   = document.createElement("script");
-externalScript.type  = "text/javascript";
-externalScript.setAttribute('async',"");
-externalScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-document.getElementById('adxadap').appendChild(externalScript);
-
-var comment = document.createComment(" infocursos_adap_jul ");
-document.getElementById('adxadap').appendChild(comment);
-
-var ins   = document.createElement("ins");
-ins.setAttribute('class','adsbygoogle');
-ins.setAttribute('display:block');
-ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
-ins.setAttribute('data-ad-slot','9303445348');
-ins.setAttribute('data-ad-format','auto');
-ins.setAttribute('data-full-width-responsive','true');
-document.getElementById('adxadap').appendChild(ins);
-
-var inlineScript   = document.createElement("script");
-inlineScript.type  = "text/javascript";
-inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
-document.getElementById('adxadap').appendChild(inlineScript);
-}());     
+  
 
     
 (function () {
