5636be4e5b79183fd93197b67ab9f1dc375f9de0
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2021-06-26 19:47:53 -0600

    1
    1

--- manual-tv-led/script.js
+++ manual-tv-led/script.js
@@ -43,7 +43,7 @@ captcha();
 function CaptchaChecked() {
 var abrirurl = document.getElementById("enlacecaptcha");
 abrirurl.onclick = function(){
-var abrirurl = window.open('https://drive.google.com/drive/folders/1Q4soQ7pl0rcToEUQljd-a7AxCBvZUW2A','_self');
+var abrirurl = window.open('https://drive.google.com/file/d/1eEUTJYiK8ZSOO3VCuT-NbO8-gxfASTOf','_self');
 }
 jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
 }
