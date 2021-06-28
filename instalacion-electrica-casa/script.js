b0f7b6a7c69186dd8db57a856897239c9674c7a0
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2021-06-28 13:30:04 -0600

    1
    1

--- instalacion-electrica-casa/script.js
+++ instalacion-electrica-casa/script.js
@@ -43,7 +43,7 @@ captcha();
 function CaptchaChecked() {
 var abrirurl = document.getElementById("enlacecaptcha");
 abrirurl.onclick = function(){
-var abrirurl = window.open('https://drive.google.com/file/d/1kjvxwVH0IWph9WTnBw7_0hFLaG-cA0Wy','_self');
+var abrirurl = window.open('https://drive.google.com/file/d/14enwoMjZd27S5ggHymvLWqBpJRFg7fW-','_self');
 }
 jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
 }
