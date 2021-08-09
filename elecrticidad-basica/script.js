b1f213f321eaa7144bf29686f35a0016099c15f8
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2021-08-09 11:49:55 -0600

    1
    1

--- elecrticidad-basica/script.js
+++ elecrticidad-basica/script.js
@@ -43,7 +43,7 @@ captcha();
 function CaptchaChecked() {
 var abrirurl = document.getElementById("enlacecaptcha");
 abrirurl.onclick = function(){
-var abrirurl = window.open('https://drive.google.com/file/d/17fMOXs1GiqHl7HcG0wW8frsibuRug9sS','_self');
+var abrirurl = window.open('https://drive.google.com/file/d/1Qk7cqpW9MzHRQbUfrc7v4G7XNvlr5ec-','_self');
 }
 jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
 }
