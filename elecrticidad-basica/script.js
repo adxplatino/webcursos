46bf752b5810cca40a257e4ab6ca5da266eb011a
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2021-08-10 07:18:32 -0600

    1
    1

--- elecrticidad-basica/script.js
+++ elecrticidad-basica/script.js
@@ -43,7 +43,7 @@ captcha();
 function CaptchaChecked() {
 var abrirurl = document.getElementById("enlacecaptcha");
 abrirurl.onclick = function(){
-var abrirurl = window.open('https://drive.google.com/file/d/1Qk7cqpW9MzHRQbUfrc7v4G7XNvlr5ec-','_self');
+var abrirurl = window.open('https://drive.google.com/file/d/1FX2JxEL5CQzrS13SPxeZbJuzCgofMZlA','_self');
 }
 jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
 }
