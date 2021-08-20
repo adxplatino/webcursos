5ff4c62dba83ce93967ea36460191a52fc66bba4
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2021-08-20 17:36:14 -0600

    1
    1

--- construccion-bano-ecologico/script.js
+++ construccion-bano-ecologico/script.js
@@ -43,7 +43,7 @@ captcha();
 function CaptchaChecked() {
 var abrirurl = document.getElementById("enlacecaptcha");
 abrirurl.onclick = function(){
-var abrirurl = window.open('https://drive.google.com/file/d/1cmqbpXsQj4-AWF9zzBcmq5Hf2QZpi7gr','_self');
+var abrirurl = window.open('https://drive.google.com/file/d/1T5IKemdtpmsIdX4Tyfgg-r3k9kMml2U1','_self');
 }
 jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
 }
