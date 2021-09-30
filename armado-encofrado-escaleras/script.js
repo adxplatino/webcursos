2f7f7f2eabb2b61fed1e9513da3ad6e6df245d89
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2021-09-30 07:31:15 -0600

    1
    1

--- armado-encofrado-escaleras/script.js
+++ armado-encofrado-escaleras/script.js
@@ -43,7 +43,7 @@ captcha();
 function CaptchaChecked() {
 var abrirurl = document.getElementById("enlacecaptcha");
 abrirurl.onclick = function(){
-var abrirurl = window.open('https://drive.google.com/drive/folders/10eWXDe0owaYkmxrDm3E9R9tcvXC5k8tK','_self');
+var abrirurl = window.open('https://drive.google.com/file/d/1ICc4HXhbtLRDArxZg_2dthdVxLQt1h9l','_self');
 }
 jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
 }
