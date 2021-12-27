1181d60919c43e0fa8a7321479a917d03fda183a
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2021-12-27 15:13:47 -0600

    1
    1

--- manual-sobre-manejo-multimetro/script.js
+++ manual-sobre-manejo-multimetro/script.js
@@ -43,7 +43,7 @@ captcha();
 function CaptchaChecked() {
 var abrirurl = document.getElementById("enlacecaptcha");
 abrirurl.onclick = function(){
-var abrirurl = window.open('https://drive.google.com/file/d/12FWdaloNflHBdrDMaZQcBN_WMf7sQizi','_self');
+var abrirurl = window.open('https://drive.google.com/file/d/1onE_iJrEZnvxkldMffFHGArUGu5QtYR1','_self');
 }
 jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
 }
