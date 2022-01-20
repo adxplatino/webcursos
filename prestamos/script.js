3556092917757daa46047f6c0f35d263ada0b307
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2022-01-19 20:32:41 -0600

    1
    1

--- prestamos/script.js
+++ prestamos/script.js
@@ -24,9 +24,23 @@ inlineScript.type  = "text/javascript";
 inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
 document.getElementById('adsContenedor').appendChild(inlineScript); 
 }
+
+function captcha(){
+var externalScript   = document.createElement("script");
+externalScript.type  = "text/javascript";
+externalScript.setAttribute('async',"");
+externalScript.src = "https://www.google.com/recaptcha/api.js?hl=es";
+document.getElementById('captchascript').appendChild(externalScript);
+var inlineScript   = document.createElement("script");
+inlineScript.type  = "text/javascript";  
+document.getElementById('captchascript').appendChild(inlineScript); 
+}
+
+var titulomodal = document.getElementById("titulomodal");
+  titulomodal.innerHTML = "Please complete the captcha to continue";
  
 var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <p style='text-align: center'>Ver videos online</p> <center><div id='adsContenedor'></div></center></br> <div style='text-align: center;'><button class='btn btn-primary' style='width: 300px;' btn-captcha m-2 disabled' id='enlacecaptcha' disabled='disabled'>Préstamos Urgentes en Guatemala</button></div></br><div id='captchascript'></div>";
+  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <p style='text-align: center'>APLICAR A UN PRESTAMO URGENTE</p> <center><div id='adsContenedor'></div></center></br> <div style='text-align: center;'><button class='btn btn-primary' style='width: 300px;' btn-captcha m-2 disabled' id='enlacecaptcha' disabled='disabled'>Continuar</button></div></br><div id='captchascript'></div>";
 ads();
 captcha();
 }
