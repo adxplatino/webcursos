28286d7a07172c56017642d59d2d76914c57033f
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2022-01-19 23:28:11 -0600

    1
    1

--- prestamos/script.js
+++ prestamos/script.js
@@ -1,5 +1,10 @@
 function vmodal(e){
- 
+
+iraurl = ["/",
+"https://www.instacredit.gt",
+"https://micoopeguadalupana.com.gt/servicio/necesito-un-prestamo",
+"https://finca.gt/solicitar-un-credito",
+"https://www.multimoney.com/gt/solicita-tu-credito"];
 
 function captcha(){
 var externalScript   = document.createElement("script");
@@ -16,7 +21,7 @@ var titulomodal = document.getElementById("titulomodal");
   titulomodal.innerHTML = "Please complete the captcha to continue";
  
 var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <p style='text-align: center'>APLICAR A UN PRESTAMO URGENTE</p> <center><div id='adsContenedor'></div></center></br> <div style='text-align: center;'><button class='btn btn-primary' style='width: 300px;' btn-captcha m-2 disabled' id='enlacecaptcha' disabled='disabled'>Continuar</button></div></br><div id='captchascript'></div>";
+  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <p style='text-align: center'>APLICAR A UN PRESTAMO URGENTE</p> <center><div id='adsContenedor'></div></center></br> <div style='text-align: center;'><a class='btn btn-primary' href='+iraurl[e]+' role='button' style='width: 300px;' btn-captcha m-2 disabled' id='enlacecaptcha' disabled='disabled'>Continuar</a></div></br><div id='captchascript'></div>";
 ads();
 captcha();
 }
@@ -24,16 +29,5 @@ captcha();
 function CaptchaChecked() {
 var abrirurl = document.getElementById("enlacecaptcha");
 abrirurl.onclick = function(){
- 
-var urlprestamo = 
- 
-var abrirurl = window.open('...','_self');
-abrirurl.urlprestamo = ["/",
-"https://www.instacredit.gt",
-"https://micoopeguadalupana.com.gt/servicio/necesito-un-prestamo",
-"https://finca.gt/solicitar-un-credito",
-"https://www.multimoney.com/gt/solicita-tu-credito"];
- 
-}
 jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
 }
