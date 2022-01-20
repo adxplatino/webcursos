65426d42aaee77b9bdf7448bbe8b0487650e4594
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2022-01-19 22:55:06 -0600

    1
    1

--- prestamos/script.js
+++ prestamos/script.js
@@ -1,24 +1,5 @@
 function vmodal(e){
  
-function ads(){
-var externalScript   = document.createElement("script");
-externalScript.type  = "text/javascript";
-externalScript.setAttribute('async',"");
-externalScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-document.getElementById('adsContenedor').appendChild(externalScript);
-var comment = document.createComment(" infoproyectos_300x250 ");
-document.getElementById('adsContenedor').appendChild(comment);
-var ins   = document.createElement("ins");
-ins.setAttribute('class','adsbygoogle');
-ins.setAttribute('data-ad-client','ca-pub-6278983531888232');
-ins.setAttribute('data-ad-slot','9029296375');
-ins.setAttribute('style','display:inline-block;width:300px;height:250px');
-document.getElementById('adsContenedor').appendChild(ins);
-var inlineScript   = document.createElement("script");
-inlineScript.type  = "text/javascript";
-inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
-document.getElementById('adsContenedor').appendChild(inlineScript); 
-}
 
 function captcha(){
 var externalScript   = document.createElement("script");
