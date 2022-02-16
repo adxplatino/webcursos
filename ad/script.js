bd65b5fb6ec25e60d89ce16d828201f1f79d08b2
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2022-02-15 22:14:06 -0600

    1
    1

--- ad/script.js
+++ ad/script.js
@@ -1,5 +1,16 @@
 function vmodal(e){
+  
+ var iraurl = ["/",
+"https://www.infoproyectos.com/p/stanford-university.html",
+"https://www.infoproyectos.com/p/university-of-pennsylvania.html",
+"https://www.infoproyectos.com/p/university-of-chicago.html",
+               
+"https://www.infoproyectos.com/p/northwestern-university.html",
+"https://www.infoproyectos.com/p/harvard-university.html",
+"https://www.infoproyectos.com/p/massachusetts-institute-of-technology.html",
  
+"https://www.infoproyectos.com/p/mba-business-schools-rankings.html"];
+  
 function ads(){
 var externalScript   = document.createElement("script");
 externalScript.type  = "text/javascript";
@@ -18,7 +29,8 @@ var inlineScript   = document.createElement("script");
 inlineScript.type  = "text/javascript";
 inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
 document.getElementById('adsContenedor').appendChild(inlineScript); 
-}
+}  
+  
 function captcha(){
 var externalScript   = document.createElement("script");
 externalScript.type  = "text/javascript";
@@ -29,18 +41,19 @@ var inlineScript   = document.createElement("script");
 inlineScript.type  = "text/javascript";  
 document.getElementById('captchascript').appendChild(inlineScript); 
 }
+
 var titulomodal = document.getElementById("titulomodal");
-  titulomodal.innerHTML = "Por favor complete el captcha para continuar";
+  titulomodal.innerHTML = "Please complete the captcha to continue";
 
 var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <p style='text-align: center'>Tarjeta de Crédito Sin Historial Crediticio</p> <center><div id='adsContenedor'></div></center></br> <div style='text-align: center;'><button class='btn btn-outline-primary btn-captcha m-2 disabled' id='enlacecaptcha' disabled='disabled'>Continuar →</button></div></br><div id='captchascript'></div>";
+  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <p style='text-align: center'>APLICAR A UN PRESTAMO URGENTE</p> <center><div id='adsContenedor'></div></center></br> <div style='text-align: center;'><a href="+iraurl[e]+" class='btn btn-primary disabled' style='width: 300px;' role='button' id='enlacecaptcha'>Continuar</a></div></br><div id='captchascript'></div>";
 ads();
 captcha();
 }
+
 function CaptchaChecked() {
 var abrirurl = document.getElementById("enlacecaptcha");
-abrirurl.onclick = function(){
-var abrirurl = window.open('https://www.infoproyectos.com/p/tarjeta-credito-sin-historial-crediticio.html','_self');
-}
-jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
+
+jQuery('#enlacecaptcha').removeClass('disabled');
+jQuery('#enlacecaptcha').addClass('active');
 }
