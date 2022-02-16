35884c025c461e2d5188d36a92b56d335f700747
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2022-02-15 23:30:37 -0600

    1
    1

--- ad/script.js
+++ ad/script.js
@@ -1,15 +1,15 @@
 function vmodal(e){
   
  var iraurl = ["/",
-"https://www.infoproyectos.com/p/stanford-university.html",
-"https://www.infoproyectos.com/p/university-of-pennsylvania.html",
-"https://www.infoproyectos.com/p/university-of-chicago.html",
+"https://www.usnews.com/best-graduate-schools/top-business-schools/stanford-university-01028",
+"https://www.usnews.com/best-graduate-schools/top-business-schools/university-of-pennsylvania-01194",
+"https://www.usnews.com/best-graduate-schools/top-business-schools/university-of-chicago-01073",
                
-"https://www.infoproyectos.com/p/northwestern-university.html",
-"https://www.infoproyectos.com/p/harvard-university.html",
-"https://www.infoproyectos.com/p/massachusetts-institute-of-technology.html",
+"https://www.usnews.com/best-graduate-schools/top-business-schools/northwestern-university-01071",
+"https://www.usnews.com/best-graduate-schools/top-business-schools/harvard-university-01110",
+"https://www.usnews.com/best-graduate-schools/top-business-schools/massachusetts-institute-of-technology-01111",
  
-"https://www.infoproyectos.com/p/usa-scholarships.html"];
+"https://www.usnews.com/education/best-colleges/paying-for-college/articles/a-guide-to-scholarships-for-international-students"];
   
 function ads(){
 var externalScript   = document.createElement("script");
@@ -46,7 +46,7 @@ var titulomodal = document.getElementById("titulomodal");
   titulomodal.innerHTML = "Please complete the captcha to continue";
 
 var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <p style='text-align: center'>Top Business Schools</p> <center><div id='adsContenedor'></div></center></br> <div style='text-align: center;'><a href="+iraurl[e]+" class='btn btn-primary disabled' style='width: 300px;' role='button' id='enlacecaptcha'>Continuar</a></div></br><div id='captchascript'></div>";
+  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <p style='text-align: center'>Top Business Schools</p> <center><div id='adsContenedor'></div></center></br> <div style='text-align: center;'><a href="+iraurl[e]+" target='_blank' rel='nofollow' class='btn btn-primary disabled' style='width: 300px;' role='button' id='enlacecaptcha'>Continuar</a></div></br><div id='captchascript'></div>";
 ads();
 captcha();
 }
