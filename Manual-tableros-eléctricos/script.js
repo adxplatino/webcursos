bf79302cd924754542a2cbeed356ad79c82f882b
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2022-02-24 15:02:07 -0600

    1
    1

--- Manual-tableros-eléctricos/script.js
+++ Manual-tableros-eléctricos/script.js
@@ -43,7 +43,7 @@ captcha();
 function CaptchaChecked() {
 var abrirurl = document.getElementById("enlacecaptcha");
 abrirurl.onclick = function(){
-var abrirurl = window.open('https://drive.google.com/file/d/1FX2JxEL5CQzrS13SPxeZbJuzCgofMZlA','_self');
+var abrirurl = window.open('https://drive.google.com/file/d/175urzSQj_OlGxzYdBgxRq-3tpRywgGuP','_self');
 }
 jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
 }
