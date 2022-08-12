6e4beb83769895d3f8afcc39ec408897111a5a31
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2022-08-11 18:29:25 -0600

    1
    1

--- busqueda/script.js
+++ busqueda/script.js
@@ -1,5 +1,5 @@
-function vmodal(urlVideo){
-  
+function linkModal(e){
+
 function ads(){
 var externalScript   = document.createElement("script");
 externalScript.type  = "text/javascript";
@@ -19,7 +19,7 @@ inlineScript.type  = "text/javascript";
 inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
 document.getElementById('adsContenedor').appendChild(inlineScript); 
 }
-  
+
 function captcha(){
 var externalScript   = document.createElement("script");
 externalScript.type  = "text/javascript";
@@ -29,28 +29,26 @@ document.getElementById('captchascript').appendChild(externalScript);
 var inlineScript   = document.createElement("script");
 inlineScript.type  = "text/javascript";  
 document.getElementById('captchascript').appendChild(inlineScript); 
-}
+};
 
 var titulomodal = document.getElementById("titulomodal");
-  titulomodal.innerHTML = "<img class='mx-auto d-block mr-3' src='https://lh3.googleusercontent.com/P4sRiiGQpoTx0-iKsaEtPvQUH7xlJGZt03gHh3YUL2s25-6hGubUCklhbfhbZSkCgS66FuMOfIVmnQJOEdHm9PF2c2YPqLwlnNiVYQ-QOw=s400-rw-no' style='height: 30px;'/>";
+titulomodal.innerHTML = "<img class='mx-auto d-block mr-3' src='https://lh3.googleusercontent.com/P4sRiiGQpoTx0-iKsaEtPvQUH7xlJGZt03gHh3YUL2s25-6hGubUCklhbfhbZSkCgS66FuMOfIVmnQJOEdHm9PF2c2YPqLwlnNiVYQ-QOw=s400-rw-no' style='height: 30px;'/>";
 
 var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <p style='text-align: center'>Por favor complete el captcha para continuar...</p> <center><div id='adsContenedor'></div></center></br> <div style='text-align: center;'><button class='btn btn-primary' style='width: 300px;' btn-captcha m-2 disabled' id='enlacecaptcha' disabled='disabled'>Continuar</button></div></br><div id='captchascript'></div>";
+cuerpomodal.innerHTML = "<div style='margin: 10px 0 10px 0;position: relative;'><div style='text-align: center;'><div class='g-recaptcha m-2' style='display: inline-block;' data-sitekey='6LeYowYaAAAAAB97jxMiic4E6kwBWjD3_Zvxdmin' data-callback='CaptchaChecked'></div></div></div> </br> <p style='text-align: center'><b>Por favor complete el captcha para continuar...</b></p> <center><div id='adsContenedor'></div></center></br><div id='captchascript'></div>";
+
 ads();
 captcha();
-getVid(urlVideo);
+getUrlModal(e);
+ 
 }
-  
-function getVid(urlVideo){
-resultadoUrl = urlVideo;
+
+function getUrlModal(e){
+resultadoUrl = e;
 }
   
 function CaptchaChecked() {
-var abrirurl = document.getElementById("enlacecaptcha");
-jQuery('#enlacecaptcha').removeClass('disabled').removeAttr('disabled');
   
-abrirurl.onclick = function(){
- 
 function ads2(){
 var externalScript   = document.createElement("script");
 externalScript.type  = "text/javascript";
@@ -70,30 +68,25 @@ inlineScript.type  = "text/javascript";
 inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
 document.getElementById('adsContenedor2').appendChild(inlineScript); 
 }
- 
-var titulomodal = document.getElementById("titulomodal");
-  titulomodal.innerHTML = "<img class='mx-auto d-block mr-3' src='https://lh3.googleusercontent.com/P4sRiiGQpoTx0-iKsaEtPvQUH7xlJGZt03gHh3YUL2s25-6hGubUCklhbfhbZSkCgS66FuMOfIVmnQJOEdHm9PF2c2YPqLwlnNiVYQ-QOw=s400-rw-no' style='height: 30px;'/>";
-  
+
 var cuerpomodal = document.getElementById("cuerpomodal");
-  cuerpomodal.innerHTML = "<div class='link-area'><div class='container'><div class='row'><div class='col-md-12 col-sm-12'><div class='contents text-center animated fadeIn' id='continue_wrapper'></br> <p style='text-align: center'>Por favor espere un momento...</p> <center><div id='adsContenedor2'></div></center></br> <div style='text-align: center;'><span class='btn btn-primary' style='width: 300px;' id='link_button'></span></div></div></br> </div></div></div></div>";
+cuerpomodal.innerHTML = "<div class='link-area'><div class='container'><div class='row'><div class='col-md-12 col-sm-12'><div class='contents text-center animated fadeIn' id='continue_wrapper'></br> <p style='text-align: center'><b>Por favor espere un momento...</b></p> <center><div id='adsContenedor2'></div></center></br> </div><div style='text-align: center;'><span class='btn btn-primary' style='width: 300px;' id='link_button'></span></div> </br> </div></div></div></div>";
 
 ads2();
-  
+
 let time_out = 1000; // 1 seconds
 let counter_id;
-let seconds = 5;
+let seconds = 10;
       
-counter_id = setInterval(update_counter_link, time_out);
-
+counter_id = setInterval(update_counter_link, time_out); 
+  
 function update_counter_link() {
 document.getElementById('link_button').innerHTML = 'Verificando: ' + (--seconds);
             if (!seconds) {
                 clearInterval(counter_id);
-                document.getElementById("link_button").innerHTML = '<div class="imps"><a rel="nofollow noreferrer" target="_blank" style="color: white;" href='+resultadoUrl+'>CONTINUAR</a></div>';
+                document.getElementById("link_button").innerHTML = '<div class="imps"><a rel="nofollow noreferrer" target="_blank" style="color: white;" href='+resultadoUrl+'><b>CONTINUAR</b></a></div>';
                 document.getElementById("link_button").style.cursor = 'default';
             }
         }
-        document.getElementById('link_button').innerHTML = 'Verificando: ' + seconds;
-}
-
+document.getElementById('link_button').innerHTML = 'Verificando: ' + seconds;
 }
