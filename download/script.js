ad446bdd637b2517ef255d0b7e916e08edeeaa63
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2023-01-10 19:27:47 -0600

    1
    1

---
+++ download/script.js
@@ -0,0 +1,15 @@
+function getLink(){
+let time_out = 1000;
+let counter_id;
+let seconds = 15;
+counter_id = setInterval(update_counter_link, time_out);
+function update_counter_link() {
+document.getElementById('link_button').innerHTML = (--seconds);
+if (!seconds) {
+clearInterval(counter_id);
+document.getElementById("contador").style.display = "none";
+document.getElementById("linkDownload").style.display = "inline";
+            }
+        }
+
+}
